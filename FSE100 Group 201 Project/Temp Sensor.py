ccfabff589c6d2fa66edaf912710d05aac936f8c
Aaditya-P21 <aadityaprakash2425@gmail.com>
2025-10-28 12:23:27 -0700

    Update Temp Sensor.py

--- FSE100 Group 201 Project/Temp Sensor.py
+++ FSE100 Group 201 Project/Temp Sensor.py
@@ -24,7 +24,7 @@ def read():
 def loop():
     while True:
         if read() != None:
-            print ("Current temperature : %0.3f C" % read())
+            print ("Current temperature : %0.2f C" % read())
 
 def destroy():
     pass
