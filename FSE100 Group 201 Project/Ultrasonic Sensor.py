fade6b7d4e0b0c3b86a02817df35fdc9433e4161
Aaditya-P21 <aadityaprakash2425@gmail.com>
2025-11-04 12:05:13 -0700

    Update Ultrasonic Sensor.py

--- FSE100 Group 201 Project/Ultrasonic Sensor.py
+++ FSE100 Group 201 Project/Ultrasonic Sensor.py
@@ -60,7 +60,7 @@ def loop():
         dis = distance()
         print(dis, 'cm')  # Print distance measurement
 
-        if dis < 15.24:  # If the object is within 5 cm, buzz continuously
+        if dis <= 15.24:  # If the object is within 5 cm, buzz continuously
             buzzer_on()
         elif dis < 30:  # If within 6 inches, beep with decreasing interval
             beep_interval = (dis - 5) / 50.0  # Adjust beep interval
